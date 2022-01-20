db3dcd4a1d4b144809459e98135a91880e44ecf9
atultaz <atul.tiwari@me.com>
2022-01-21 00:09:09 +0530

    backend complete

--- YoutubeClone/App.tsx
+++ YoutubeClone/App.tsx
@@ -1,21 +1,54 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import Amplify, { Auth, DataStore } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react-native';
+import config from './src/aws-exports';
+Amplify.configure(config);
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import VideoScreen from './screens/VideoScreen';
 
-import Amplify, { Auth } from 'aws-amplify';
-import { withAuthenticator } from 'aws-amplify-react-native';
-import awsconfig from './src/aws-exports';
-Amplify.configure(awsconfig);
+import { User } from './src/models';
 
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    const saveUserToDB = async () => {
+      // get user  from cognito
+      const userInfo = await Auth.currentAuthenticatedUser();
+
+      if (!userInfo) {
+        return;
+      }
+      const userId = userInfo.attributes.sub;
+
+      // check if user exists in DB
+      const user = (await DataStore.query(User)).find(
+        (user) => user.sub === userId
+      );
+      if (!user) {
+        // if not, save user to db.
+        await DataStore.save(
+          new User({
+            sub: userId,
+            name: userInfo.attributes.email,
+            subscribers: 0,
+          })
+        );
+      } else {
+        console.warn('User already exists in DB');
+      }
+    };
+
+    saveUserToDB();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
